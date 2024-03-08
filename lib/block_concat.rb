365903e3cda7f5da6e9ebcea3d4021b748ce060c
Maria <mvputrova@gmail.com>
2024-03-08 16:54:16 +0300

    Task 2 done

--- lib/block_concat.rb
+++ lib/block_concat.rb
@@ -2,5 +2,15 @@
 # @param [String] b
 # @param [Integer] n
 def block_concat(a, b, n)
-  nil
+  res = ''
+
+  if a == '' and b == ''
+    res
+  end
+
+  until a.size == 0 and b.size == 0
+    res += a.slice!(0, n) + b.slice!(0, n)
+  end
+
+  res
 end
