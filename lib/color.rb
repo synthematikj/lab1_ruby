70aa9c210c2ee07ae2b69b2c5d7c1a19f1183856
Maria <mvputrova@gmail.com>
2024-03-08 16:38:18 +0300

    Task 1 done

--- lib/color.rb
+++ lib/color.rb
@@ -2,6 +2,21 @@
 # @param [Integer] g
 # @param [Integer] b
 def color(r, g, b)
-  # 100.to_s(16) => 64
-  nil
+  result = ''
+
+  if r or b or g < 0
+    r = [r, 0].max
+    g = [g, 0].max
+    b = [b, 0].max
+  end
+
+  if r or b or g > 255
+    puts "dada"
+    r = [r, 255].min
+    g = [g, 255].min
+    b = [b, 255].min
+  end
+
+  result = (r.to_s(16) + g.to_s(16) + b.to_s(16)).upcase
+  result.gsub('0','00')
 end
