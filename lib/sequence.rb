36fd6e18bba4345291a8042ac6be86aa68937ebf
Maria <mvputrova@gmail.com>
2024-03-08 17:14:41 +0300

    Task 3 done

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -1,5 +1,19 @@
 # @param [String] s
 def sequence(s)
-  nil
-end
+  s = s.downcase
 
+  count = 1
+  max = 0
+  (0...s.length).each do |i|
+    if s[i] == s[i+1]
+      count += 1
+
+    else
+      if count > max
+        max = count
+      end
+      count = 1
+    end
+  end
+  max
+end
